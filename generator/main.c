7dfd23c8b8c17cb82802e9783fcfdad4f0d47347
Eliot Janvier <eliot.janvier@epitech.eu>
2022-03-07 16:50:43 +0100

    [UPDATE] generating imperfects mazes

--- generator/main.c
+++ generator/main.c
@@ -5,7 +5,16 @@
 ** main.c
 */
 
-int main(int argc, char **argv) {
+#include <stdlib.h>
+#include <stdio.h>
 
+#include "structs.h"
+#include "gen.h"
+
+int main(int argc, char **argv)
+{
+    maze_builder_t *my_maze = set_maze_builder(atoi(argv[1]), atoi(argv[2]));
+    generate_map(my_maze);
+    destroy_maze_builder(my_maze);
     return 0;
 }
